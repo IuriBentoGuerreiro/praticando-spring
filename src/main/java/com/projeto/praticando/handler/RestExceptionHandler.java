ab55a410f5c91f96690816c0cc1724ed51958d04
iuri <bentoiuri@gmail.com>
2024-07-16 17:23:42 -0300

    organiza excecoes

--- src/main/java/com/projeto/praticando/handler/RestExceptionHandler.java
+++ src/main/java/com/projeto/praticando/handler/RestExceptionHandler.java
@@ -1,72 +1,23 @@
 package com.projeto.praticando.handler;
 
-import com.projeto.praticando.excecao.BadRequestException;
-import com.projeto.praticando.excecao.ExceptionResponse;
 import com.projeto.praticando.excecao.NotFoundException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.http.HttpHeaders;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.naming.ServiceUnavailableException;
-import java.lang.reflect.Method;
-
-@Slf4j
 @RestControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler
-        implements AsyncUncaughtExceptionHandler{
-
-    @Override
-    public void handleUncaughtException(Throwable throwable, Method method, Object... objects) {
-        log.info("Erro async: " + throwable.getMessage());
-        throwable.printStackTrace();
-    }
-
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(String.format("O campo %s %s", ex.getFieldError().getField(),
-                        ex.getFieldError().getDefaultMessage()), ex.getFieldError().toString());
-
-        return new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(BadRequestException.class)
-    public final ResponseEntity<ExceptionResponse> handlerBadRequest(
-            BadRequestException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
-    }
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public final ResponseEntity<ExceptionResponse> handlerNotFound(
-            NotFoundException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    private ResponseEntity<String> notFoundExceptionHandler(NotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
-    @ExceptionHandler(ServiceUnavailableException.class)
-    public final ResponseEntity<ExceptionResponse> handlerServiceUnavailable(
-            ServiceUnavailableException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.SERVICE_UNAVAILABLE);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ExceptionResponse> handlerAllException(
-            Exception ex, WebRequest request) {
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(ConstraintViolationException.class)
+    private ResponseEntity<String> constraintViolationExceptionHandler(ConstraintViolationException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 }
